bf10a1892af935e72b93b0fab3705e2070d42298
Ricardo <rpenalozav@unal.edu.co>
2022-02-16 11:54:33 -0500

    comunication using channels

--- concurrency/sort_channel.go
+++ concurrency/sort_channel.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
+func MySort(l []int, c chan []int) {
+	sort.Ints(l)
+	c <- l
+}
+
 func main() {
-	fmt.Println("Enter numbers for order")
+	c := make(chan []int)
+	num := make([]int, 0)
+	aux := make([]int, 0)
+	var a, count int
+	fmt.Println("Enter amount of numbers: ")
+	fmt.Scan(&count)
+	if count < 4 {
+		panic("min 4 numbers")
+	}
+
+	for i := 0; i < count; i++ {
+		fmt.Scan(&a)
+		num = append(num, a)
+	}
+	size := len(num) / 4
+	go MySort(num[:size], c)
+	go MySort(num[size:size*2], c)
+	go MySort(num[size*2:size*3], c)
+	go MySort(num[size*3:], c)
+
+	for i := 0; i < 4; i++ {
+		x := <-c
+		aux = append(aux, x...)
+	}
+	sort.Ints(aux)
+	fmt.Println(aux)
 }
